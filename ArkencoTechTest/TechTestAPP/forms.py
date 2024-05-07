828058190dd1b578dcde55b8565babb099a12c8f
Blap00 <fa.palma@duocuc.cl>
2024-05-07 18:16:34 -0400

    Set Loggin Method!

---
+++ ArkencoTechTest/TechTestAPP/forms.py
@@ -0,0 +1,66 @@
+#Typing
+from typing import Any
+# USUARIOS DJANGO CONTRIB
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, SetPasswordForm, PasswordResetForm
+
+# SET FORMS
+from django import forms
+
+# Models Default
+from .models import Usuarios 
+# USER LOGIN
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Username or Email'}),
+        label="Username or Email*")
+
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    
+# User REGISTER
+class CustomUserCreationForm(UserCreationForm):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        # Add Email field
+        self.fields['email'] = forms.EmailField()
+        self.fields['email'].label = "Direccion de Email: "
+        self.fields['email'].widget.attrs.update({'class': 'form-control'})
+
+        # Add Username field
+        self.fields['username'] = forms.CharField()
+        self.fields['username'].label = "Nombre de usuario: "
+        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+
+        # Add first_name field
+        self.fields['first_name'] = forms.CharField()
+        self.fields['first_name'].label = "Nombre: "
+        self.fields['first_name'].widget.attrs.update({'class': 'form-control'})
+
+        # Add last_name field
+        self.fields['last_name'] = forms.CharField()
+        self.fields['last_name'].label = "Apellido: "
+        self.fields['last_name'].widget.attrs.update({'class': 'form-control'})
+
+        # Add password1 field
+        self.fields['password1'] = forms.CharField(widget=forms.PasswordInput)
+        self.fields['password1'].label = "Contraseña: "
+        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
+
+        # Add password2 field
+        self.fields['password2']   = forms.CharField(widget=forms.PasswordInput)
+        self.fields['password2'].label = "Repita la contraseña: "
+        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
+
+        
+
+    class Meta(UserCreationForm.Meta):
+            model=Usuarios
+            fields=('email',
+            'username', 
+            'first_name', 
+            'last_name', 
+            'password1', 
+            'password2')
--- ArkencoTechTest/TechTestAPP/views.py
+++ ArkencoTechTest/TechTestAPP/views.py
@@ -1,7 +1,61 @@
-from django.shortcuts import render
+# Shortcuts from Django to render, redirect, etc...
+from django.shortcuts import render, redirect
+
+# Decorators, if user is logged or not
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from .decorators import user_not_authenticated
+# Forms
+
+from .models import Usuarios as users
+from .forms import *
+
+
+# Models
 
 # Create your views here.
 
+# Login
+@user_not_authenticated
+def loginUser(request):
+    if request.method == "POST":
+        form = UserLoginForm(request=request, data=request.POST)
+        def get_username_by_mail(email):
+                try:
+                    username = users.objects.get(email=email).username
+                    return username
+                except:
+                    username= email
+                    return email 
+        mutable_data = form.data.copy()
+        mutable_data['username'] = get_username_by_mail(form.data["username"])
+        form.data = mutable_data
+        if form.is_valid():
+            
+            user = authenticate(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password"],
+            )
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Bienvenido <b>{user.username}</b>! Has iniciado sesión")
+                return redirect('../../')
+            else:
+                pass
+        else:
+            for key, error in list(form.errors.items()):
+                if key == 'captcha' and error[0] == 'This field is required.':
+                    messages.error(request, "You must pass the reCAPTCHA test")
+                    continue                
+                messages.error(request, error) 
+
+    form = UserLoginForm()
+    return render(
+        request=request,
+        template_name="registration/login.html",
+        context={"form": form}
+        )
+# Index
 def home_view(request):
-    print(request.user.is_staff)
     return render(request,'index.html')
